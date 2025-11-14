6e45e32287aa1311e2aadd0d63df09494cba1622
Valeria <etoile.eclat.sans.arret358@gmail.com>
2025-11-14 20:43:05 +0900

    Initial commit: React tic-tac-toe project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function Board() {
+  return (
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
